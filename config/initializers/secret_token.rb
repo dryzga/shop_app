05b372c6550d9a996eeeed1e26c26a7ea21057b3
Dawid Drzala <dryzga@dryzga-VirtualBox.(none)>
2012-10-12 16:13:12 +0200

    zaczynamy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopApp::Application.config.secret_token = '85ce467d7ccc3a4c59d21bb32190501d1cf9a0b1eec55063da4a6897352f6f5d8d4042c657a265e5486625c6b407e543c410a8056a373c2584ce5afcc0673943'
