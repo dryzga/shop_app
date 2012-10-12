be4b3c8bbdd9e2a44e54a8c162a1637dc2a2abb2
Dawid Drzala <dryzga@dryzga-VirtualBox.(none)>
2012-10-12 18:21:41 +0200

    Part 1 - Welcome on my shop

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,5 @@
+class ShopController < ActionController::Base
+	def index
+		render :text => "hello in my shop"
+	end
+  end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopApp::Application.config.secret_token = 'd394a6129427839801bc7e80ae51542f6cafe32d82ca150c3454bf20d26d3979b9d03d4489597b48ff7d5c1834459b410c6a72e4a6d93e02256fb9ad449e4c8c'
