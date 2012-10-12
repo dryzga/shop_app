386b9f3ca1dc39c7723f3f17805ac6cca9eb47b8
Dawid Drzala <dryzga@dryzga-VirtualBox.(none)>
2012-10-12 18:38:06 +0200

    error in comit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,8 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-<<<<<<< HEAD
-ShopApp::Application.config.secret_token = 'd394a6129427839801bc7e80ae51542f6cafe32d82ca150c3454bf20d26d3979b9d03d4489597b48ff7d5c1834459b410c6a72e4a6d93e02256fb9ad449e4c8c'
-=======
+
 ShopApp::Application.config.secret_token = '85ce467d7ccc3a4c59d21bb32190501d1cf9a0b1eec55063da4a6897352f6f5d8d4042c657a265e5486625c6b407e543c410a8056a373c2584ce5afcc0673943'
->>>>>>> 05b372c6550d9a996eeeed1e26c26a7ea21057b3
+
