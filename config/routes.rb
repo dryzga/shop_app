cdcabf7406add53f42e1f8b4ba6ba9eea7ce4d5d
Dawid Drzala <dryzga@dryzga-VirtualBox.(none)>
2012-10-13 11:42:35 +0200

    Part 2 Done

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ ShopApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  resource :shop
+  resource :shop 
+  resources :products
+  match "/products/:id" => "products#show"
   root :to => 'shop#index'
 end
